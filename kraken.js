6cebee5b6f0502472d912dbe221a3a7744da7f01
SethBoadi <99733037+SethBoadi@users.noreply.github.com>
2024-07-20 09:19:13 +0000

    Initial commit1

--- kraken.js
+++ kraken.js
@@ -4,20 +4,7 @@ const crypto = require('crypto');
 const querystring = require('querystring');  
 
 const API_KEY = 'oildollar'; // Replace with your actual API key  
-const API_SECRET = 'iQIzBAABCAAdFiEEPupNg1gu2wWnBIG0o4BC9gfWI9oFAmaZn38ACgkQo4BC9gfW
-I9oX7BAArSFZ9zjyhgx3yA4ByrkNNU6tyzpnhguRjazPpbm+ksF+retpc7XUy55h
-bEtVHqlgbbiec/vrogkhN1G4RBjDTTSQ+Al35rWbCtf0LX+f9j8BtIyG2L+6SGVV
-SpnfE/EF6p3l7cZSDN6JZHDy44HKm+NgS84Ziw2nd1tnykz1ANaerhb9LSUhu+PW
-O6jccqcrFhsD3Y/N4fjD2XbhQWMoeNId2kaH2qB6nmlnz7MAAdFy01ccg7Hilm7B
-BEItWpPUjdHVuewo9oBrv4E3Sc6sS75ZW4oqKVYsEFo7o1htfW8cub9g3tv47o9M
-8p6PhzUBBw3RtLSnGnorw99oicVuAsaPlw/9sv7PRSKd2Hnj6piOBjpQbqbm6bkU
-mPipXzMDTj5RoyxntuK1iw7um5VI8KBe4vJNE2+/vhu8Xk3VRd0P2HQnqLgwXqSu
-3Hv19v/H8yFqZELcAjJBR047GWhhmutX/WZzZNFfG2mgpLa4wMoWLTE11DWPAOGk
-xw4Om1HqZaSntIoVdsBFdrYL2zvhXxe8CwVnz0e4L2OGeDx/AP74BB5vOG2LQaIO
-zEau7lUweiQDi7MfcVyNzn7TvBYEQB1qnxc0+Wk+Iq/jITYwjBug6Iycr1YszR/Q
-SwttoBiiXLkgTXoGBPtGJZRcGidyQPVb0ZMVWOuou6+8eXfs6L4=
-=z9jP
-'; // Replace with your actual API secret  
+const API_SECRET = 'iQIzBAABCAAdFiEEPupNg1gu2wWnBIG0o4BC9gfWI9oFAmaZn38ACgkQo4BC9gfWI9oX7BAArSFZ9zjyhgx3yA4ByrkNNU6tyzpnhguRjazPpbm+ksF+retpc7XUy55hbEtVHqlgbbiec/vrogkhN1G4RBjDTTSQ+Al35rWbCtf0LX+f9j8BtIyG2L+6SGVVSpnfE/EF6p3l7cZSDN6JZHDy44HKm+NgS84Ziw2nd1tnykz1ANaerhb9LSUhu+PWO6jccqcrFhsD3Y/N4fjD2XbhQWMoeNId2kaH2qB6nmlnz7MAAdFy01ccg7Hilm7BBEItWpPUjdHVuewo9oBrv4E3Sc6sS75ZW4oqKVYsEFo7o1htfW8cub9g3tv47o9M8p6PhzUBBw3RtLSnGnorw99oicVuAsaPlw/9sv7PRSKd2Hnj6piOBjpQbqbm6bkUmPipXzMDTj5RoyxntuK1iw7um5VI8KBe4vJNE2+/vhu8Xk3VRd0P2HQnqLgwXqSu3Hv19v/H8yFqZELcAjJBR047GWhhmutX/WZzZNFfG2mgpLa4wMoWLTE11DWPAOGkxw4Om1HqZaSntIoVdsBFdrYL2zvhXxe8CwVnz0e4L2OGeDx/AP74BB5vOG2LQaIOzEau7lUweiQDi7MfcVyNzn7TvBYEQB1qnxc0+Wk+Iq/jITYwjBug6Iycr1YszR/QSwttoBiiXLkgTXoGBPtGJZRcGidyQPVb0ZMVWOuou6+8eXfs6L4==z9jP'; // Replace with your actual API secret  
 const BASE_URL = 'https://api.kraken.com';  
 
 function getSignature(path, request, nonce) {  
